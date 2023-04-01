91932cd54a06802ac7ef92b173d2b765c6dc279c
julian <jullianvanegas19@gmail.com>
2023-03-31 23:27:50 -0500

    quinto intento

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
-import cancion2 from '../audios/y2mate.com - Maycol Riddim Ft Ryan Castro  Nunca Imagine Prod Palma Productions_320kbps.mp3';
+import cancion2 from '../audios/cancion.mp3';
 
 export const Audio = () => {
   return <ReactAudioPlayer src={cancion2} autoPlay={true} controls={true} />;
